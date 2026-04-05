5acf86b3cdffcaa3287412f175c8e12f69e22287
akpalanaza <91196264+Retr0Kr0dy@users.noreply.github.com>
2026-04-05 11:02:38 +0200

    Add shift and modifier masks to keyboard config

--- src/boards/board_cardputer.c
+++ src/boards/board_cardputer.c
@@ -4,12 +4,16 @@
 /* Defined in src/drivers/input/cardputer_keymap.c */
 extern char sic_keymap_cardputer(int idx);
 
-static const sic_kscan_cfg_t g_kbd_cfg = { .keymap = sic_keymap_cardputer };
+/* Bit 3 = SHIFT key (row 3, col 0 in keymap), bit 2 = FN/Alt key (row 2, col 0) */
+static const sic_kscan_cfg_t g_kbd_cfg = {
+    .keymap        = sic_keymap_cardputer,
+    .shift_mask    = (1ULL << 3),
+    .modifier_mask = (1ULL << 2),
+};
 
 static const struct sic_board_ic_s g_ics[] = {
     { SIC_F_KSCAN,   "kbd_74hc138", &g_kbd_cfg },
     { SIC_F_CHARGER, "tp4057",      NULL       },
-    { SIC_F_KSCAN,   "dummy",       NULL       }
 };
 
 const struct sic_board_s SIC_BOARD_CARDPUTER = {
